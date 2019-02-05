c5ebb7a37f2236e51e0d77a37bf5543ed6b15602
sj <sjcomeau@wpi.edu>
2019-02-05 17:26:59 -0500

    adding comments and making it more understandable

--- secure_server.py
+++ secure_server.py
@@ -1,35 +1,36 @@
-import sys
+import os, argparse
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 import BaseHTTPServer
 
 
-def test(HandlerClass=SimpleHTTPRequestHandler,
-         ServerClass=BaseHTTPServer.HTTPServer):
+def open_http(host, port, directory):
+    
+    web_path = os.path.abspath(directory)
+    os.chdir(web_path)
 
     protocol = "HTTP/1.0"
-    host = ''
-    port = 7001
-    if len(sys.argv) > 1:
-        arg = sys.argv[1]
-        if ':' in arg:
-            host, port = arg.split(':')
-            port = int(port)
-        else:
-            try:
-                port = int(sys.argv[1])
-            except:
-                host = sys.argv[1]
 
-    server_address = (host, port)
-
-    HandlerClass.protocol_version = protocol
-    ServerClass.allow_reuse_address=True
-    httpd = ServerClass(server_address, HandlerClass)
+    SimpleHTTPRequestHandler.protocol_version = protocol
+    httpd = BaseHTTPServer.HTTPServer((host, port), SimpleHTTPRequestHandler)
 
     sa = httpd.socket.getsockname()
-    print "Serving HTTP on", sa[0], "port", sa[1], "..."
-    httpd.serve_forever()
+    print "Serving HTTP for ", web_path, " at ", host, ":", port, "..."
+    
+    try:    
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.socket.close()
 
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--host', default='127.0.0.1', help='The host for the server.')
+    parser.add_argument('-p', '--port', type=int, default=7001, help='The port to serve HTTP on.')
+    parser.add_argument('-d', '--dir', default='./', help='The directory that holds the contents of the site.')
+
+    args = parser.parse_args()
+
+    open_http(args.host, args.port, args.dir)
 
 if __name__ == "__main__":
-    test()
+    main()
